b6343d6c35ff05e9feff16567c9a214a869f0921
Lucas Dutra <lucas.word.dutra@gmail.com>
2026-01-19 20:59:27 -0300

    modificando tela about, adicionando competencias e certificados

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".nav-link");
 
-const observer3 = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      navLinks.forEach(link => link.classList.remove("active"));
-      const activeLink = document.querySelector(`.nav-link[href="#${entry.target.id}"]`);
-      activeLink.classList.add("active");
-    }
-  });
-}, { threshold: 0.4 });
+//Animação das nav-links
 
-sections.forEach(section => observer3.observe(section));
+const observer2 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        const activeLink = document.querySelector(
+          `.nav-link[href="#${entry.target.id}"]`,
+        );
+        if (activeLink) {
+          activeLink.classList.add("active");
+        }
+      }
+    });
+  },
+  { threshold: 0.4 },
+);
+
+sections.forEach((section) => observer2.observe(section));
+
+//Animação typing da hero
 
 const textElement = document.querySelector(".typing-text");
 const texts = ["Full Stack", "Frontend", "Backend"];
@@ -46,75 +57,22 @@ function typeEffect() {
 
 typeEffect();
 
+//Animação da seção sobre
+
 const aboutAnimation = document.querySelectorAll(".about-container");
 
-const observer = new IntersectionObserver((entries) =>{
-  entries.forEach(entry => { 
-    if(entry .isIntersecting){
-      entry.target.classList.add("active");
-    } else{
-      entry.target.classList.remove("active");
-    }
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  },
+  { threshold: 0.35 },
+);
 
-  });
-}, {threshold: 0.4});
-
-
-aboutAnimation.forEach(section => observer.observe(section));
-
-
-const sectionSobre = document.querySelector("#about-section");
-
-const observer2 = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const skills = sectionSobre.querySelectorAll(".skill");
-
-      skills.forEach(skill => {
-        const percent = skill.dataset.percent / 100;
-        const progress = skill.querySelector(".progress");
-        const value = skill.querySelector(".progress-value");
-
-        progress.style.setProperty("--scale", percent);
-        skill.classList.add("active");
-
-        const target = parseInt(skill.dataset.percent);
-        let start = null;
-
-        function animate(timestamp) {
-          if (!start) start = timestamp;
-          const elapsed = timestamp - start;
-
-          const duration = 2000;
-          const progressPercent = Math.min((elapsed / duration) * target, target);
-
-          value.textContent = Math.round(progressPercent) + "%";
-
-          if (progressPercent < target) {
-            requestAnimationFrame(animate);
-          }
-        }
-
-        requestAnimationFrame(animate);
-      });
-
-    } else {
-     
-      const skills = sectionSobre.querySelectorAll(".skill");
-      skills.forEach(skill => {
-        const progress = skill.querySelector(".progress");
-        const value = skill.querySelector(".progress-value");
-
-        progress.style.setProperty("--scale", 0);
-        skill.classList.remove("active");
-        value.textContent = "0%"; 
-      });
-    }
-  });
-}, { threshold: 0.35
- });
-
-observer2.observe(sectionSobre);
-
-const navLink = document.querySelector(".nav-link");
+aboutAnimation.forEach((section) => observer.observe(section));
 
